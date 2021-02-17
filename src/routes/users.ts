6533014c35142eaee423dea636e0f78845744733
Simon Julius <simonjulius0511@gmail.com>
2021-02-17 19:14:21 +0100

    setup for lintin completed

---
+++ src/routes/users.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+const router = express.Router()
+
+/* GET users listing. */
+router.get('/', function (req: any, res: any) {
+  res.send('respond with a resource')
+})
+
+export default router
